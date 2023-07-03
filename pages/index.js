0425e00eff6ed62604a6ff33635e5a35f10e997d
go2dev2023 <137208441+go2dev2023@users.noreply.github.com>
2023-07-03 21:37:23 +0000

    Adionada a pasta Pages

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+
+function Home() {
+  return (
+    <div>
+      <h1>Meu Amor..... &#x2764;&#x2764;&#x2764;</h1>
+      <h2>Eu e o Marcos amamos você do tamanho do infinito!!! &#x2764;&#x1F436;&#x1F469;&#x200D;&#x2764;&#xFE0F;&#x200D;&#x1F468;&#x2764;</h2>
+    </div>
+  );
+}
+
+export default Home;
